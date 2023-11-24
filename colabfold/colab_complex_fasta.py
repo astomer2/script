62982225f2689d1fca3664ed1d7a4bc5dfdeeb89
astomer2 <1270545485@qq.com>
2023-11-24 15:08:26 +0800

    test

--- colabfold/colab_complex_fasta.py
+++ colabfold/colab_complex_fasta.py
@@ -1,35 +1,44 @@
 
 import os
 
-protein_file = "/mnt/nas1/lanwei-125/IL8/v4/IL8dimer.txt"
-peptides_file = "/mnt/nas1/lanwei-125/IL8/v4/seq.txt"
-output_dir = "/mnt/nas1/lanwei-125/IL8/v4/AF/input/"
 
-# 读取蛋白序列
-with open(protein_file) as f:
-    protein_seq = ''
-    for line in f:
-        if not line.startswith('>'):
-            protein_seq += line.strip()
 
-# 读取多肽序列  
-with open(peptides_file) as f:
-    peptide_seqs = []
-    for line in f:
-        if not line.startswith('>'):
-            peptide_seqs.append(line.strip())
+def read_protein_fasta(file):
+    with open(file) as f:
+        protein_seq = ''
+        for line in f:
+            if not line.startswith('>'):
+                protein_seq += line.strip()
+    return protein_seq
+
+def read_peptide_fasta(file):
+    with open(file) as f:
+        peptide_seqs = []
+        for line in f:
+            if not line.startswith('>'):
+                peptide_seqs.append(line.strip())
+    return peptide_seqs
 
 # 为每个多肽序列生成新的fasta文件
-for peptide in peptide_seqs:
-    outfile = os.path.join(output_dir, peptide + '_dimer.fasta')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    with open(outfile, 'w') as f:
-        f.write('>'+ peptide+'\n')
-        f.write(protein_seq + ':\n')
-        f.write( peptide)
+def write_peptide_fasta(peptide_seqs, protein_seq, output_dir):
+    for peptide in peptide_seqs:
+        outfile = os.path.join(output_dir, peptide + '.fasta')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        with open(outfile, 'w') as f:
+            f.write('>'+ peptide+'\n')
+            f.write(protein_seq + ':\n')
+            f.write( peptide)
 
+def main(protein_file, peptides_file, output_dir):
+    protein_seq = read_protein_fasta(protein_file)
+    peptide_seqs = read_peptide_fasta(peptides_file)
+    write_peptide_fasta(peptide_seqs, protein_seq, output_dir)
+    print('Done. Output files written to', output_dir)
 
-print('Done. Output files written to', output_dir)
-
+if __name__ == '__main__':
+    protein_file = "/mnt/nas1/lanwei-125/FGF5/remodel_generated_cycpeptide/FGF5.fasta"
+    peptides_file = "/mnt/nas1/lanwei-125/FGF5/remodel_generated_cycpeptide/fgf5_peptide.txt"
+    output_dir = "/mnt/nas1/lanwei-125/FGF5/remodel_generated_cycpeptide/input/"
+    main(protein_file, peptides_file, output_dir)
 
