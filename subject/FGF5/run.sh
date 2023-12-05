d366d5a1d8d42b6af2e9505ada4d4ea544309b03
astomer2 <1270545485@qq.com>
2023-12-05 17:40:29 +0800

    u

--- subject/FGF5/run.sh
+++ subject/FGF5/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # 定义参数
-WORK_PATH="/mnt/nas1/lanwei-125/FGF5/FGF5-pos/new_pos/MD/pos/CYMAEYMYC"
+WORK_PATH="/mnt/nas1/lanwei-125/FGF5/Dis_to_cov/CC_ro_AM/MD/ANSIGM/"
 log_path=$WORK_PATH"/log.txt"
-CUDA_DEVICE_ID=3 
-PARAMETER_FILE="/mnt/sdc/lanwei/script-1/MD/amber_paramter_files"
-CMAP_PATH="/mnt/sdc/lanwei/script-1/MD/CMAP_files"
+CUDA_DEVICE_ID=0 
+
  
 
 
-nohup python /mnt/sdc/lanwei/script-1/MD/run-simulation.py -i "$WORK_PATH" -g "$CUDA_DEVICE_ID" -CAMP 1 -cys 1 > "$log_path" 2>&1 & 
+nohup python /mnt/sdc/lanwei/script-1/MD/run-simulation.py -i "$WORK_PATH" -g "$CUDA_DEVICE_ID"  -cyc 1 > "$log_path" 2>&1 & 
